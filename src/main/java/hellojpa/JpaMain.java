d969ba9c8fa95365dc8c25f36c5283188ee2a742
alpacaive <w6352800@naver.com>
2024-07-23 23:09:32 +0900

    트랜잭션을 지원하는 쓰기 지연

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,25 +15,15 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            // 비영속
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloJPA");
-//
-//            // 영속 -> 여기서 DB에 쿼리문이 날아가지 않음
-//            System.out.println("=== BEFORE ===");
-//            em.persist(member);
-//            System.out.println("=== AFTER ===");
 
-            // 1차 캐시에서 조회
-            Member findMember1 = em.find(Member.class, 101L); // DB에서 가져옴 = 쿼리문 날아감
-            Member findMember2 = em.find(Member.class, 101L); // 1차 캐시에서 꺼내옴, 결론 : 쿼리 1번만 쏴야함
+            // 영속
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
 
-            // 영속 엔티티의 동일성 보장
-            System.out.println("result = " + (findMember1 == findMember2)); // 동일성 비교 true
+            em.persist(member1);
+            em.persist(member2);
 
-//            System.out.println("findMember.getId() = " + findMember.getId());
-//            System.out.println("findMember.getName() = " + findMember.getName());
+            System.out.println("=========================");
 
             tx.commit();
         } catch (Exception e) {
