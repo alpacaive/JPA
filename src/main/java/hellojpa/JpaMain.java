85519575a679ca9e726f7b7ca36d571bf48e3800
alpacaive <w6352800@naver.com>
2024-07-23 23:39:52 +0900

    flush

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,11 +18,11 @@ public class JpaMain {
 
             // 영속
 
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
-//            em.persist(member); 오히려 이걸 하면 안됨, 자바 컬렉션을 다루듯 값만 바꾸면 됨
-            // JPA는 변경 감지라는 기능으로 엔티티를 변경할 수 있게 기능이 제공됨
-            // 영속 컨텍스트에서 데이터 원본을 스냅샷을 떠놔서 엔티티랑 비교해서 바뀌었으면 update 쿼리를 쏴줌
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+
+            em.flush(); // insert 쿼리 커밋 전에 볼 수 있음
+            // flush 해도 1차 캐시는 지워지지 않음
 
             System.out.println("====================");
 
