0093dbec2a985fcd24cacd8d6d167aa698c2b2bf
alpacaive <w6352800@naver.com>
2024-07-24 18:44:38 +0900

    실전 예제1 - 요구사항 분석과 기본 매핑 데이터 중심 설계의 문제점 - 현재 방식은 객체 설계를 테이블 설계에 맞춘 방식 - 테이블의 외래키를 객체에 그대로 가져옴 - 객체 그래프 탐색이 불가능 - 참조가 없으므로 UML도 잘못됨

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,49 +1,49 @@
-package hellojpa;
-
-import jakarta.persistence.*;
-
-import java.util.List;
-
-public class JpaMain {
-
-    public static void main(String[] args) {
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 트랜잭션 안에서 되어야 한다
-
-        tx.begin();
-
-        try {
-
-            Member member1 = new Member();
-            member1.setUsername("A");
-
-            Member member2 = new Member();
-            member2.setUsername("B");
-
-            Member member3 = new Member();
-            member3.setUsername("C");
-
-            System.out.println("====================");
-
-            em.persist(member1); // 1, 51
-//            em.persist(member2); // MEMORY
-//            em.persist(member3); // MEMORY
-
-            System.out.println("member1.id = " + member1.getId());
-            System.out.println("member2.id = " + member2.getId());
-            System.out.println("member3.id = " + member3.getId());
-
-            System.out.println("====================");
-
-            tx.commit();
-        } catch (Exception e) {
-            tx.rollback();
-        } finally {
-            em.close();
-        }
-
-        emf.close();
-    }
-}
+//package hellojpa;
+//
+//import jakarta.persistence.*;
+//
+//import java.util.List;
+//
+//public class JpaMain {
+//
+//    public static void main(String[] args) {
+//
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+//        EntityManager em = emf.createEntityManager();
+//        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 트랜잭션 안에서 되어야 한다
+//
+//        tx.begin();
+//
+//        try {
+//
+//            Member member1 = new Member();
+//            member1.setUsername("A");
+//
+//            Member member2 = new Member();
+//            member2.setUsername("B");
+//
+//            Member member3 = new Member();
+//            member3.setUsername("C");
+//
+//            System.out.println("====================");
+//
+//            em.persist(member1); // 1, 51
+////            em.persist(member2); // MEMORY
+////            em.persist(member3); // MEMORY
+//
+//            System.out.println("member1.id = " + member1.getId());
+//            System.out.println("member2.id = " + member2.getId());
+//            System.out.println("member3.id = " + member3.getId());
+//
+//            System.out.println("====================");
+//
+//            tx.commit();
+//        } catch (Exception e) {
+//            tx.rollback();
+//        } finally {
+//            em.close();
+//        }
+//
+//        emf.close();
+//    }
+//}
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,50 +1,50 @@
-package hellojpa;
-
-import jakarta.persistence.*;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-
-@Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 50 // DB에 미리 50개 올려놓고 그 개수만큼 미리 쓰는 방식, 여러 웹 서버가 있어도 동시성 이슈 없이 다양한 문제들이 해결됨
-)
-//@TableGenerator(
+//package hellojpa;
+//
+//import jakarta.persistence.*;
+//
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import java.util.Date;
+//
+//@Entity
+//@SequenceGenerator(
 //        name = "MEMBER_SEQ_GENERATOR",
-//        table = "MY_SEQUENCES",
-//        pkColumnValue = "MEMBER_SEQ", allocationSize = 1
+//        sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이름
+//        initialValue = 1, allocationSize = 50 // DB에 미리 50개 올려놓고 그 개수만큼 미리 쓰는 방식, 여러 웹 서버가 있어도 동시성 이슈 없이 다양한 문제들이 해결됨
 //)
-public class Member {
-
-    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
-//    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
-    private Long id;
-
-    @Column(name = "name", nullable = false) // DB 컬럼명은 name, 객체 이름은 username으로 사용
-    private String username;
-
-    public Member() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-}
+////@TableGenerator(
+////        name = "MEMBER_SEQ_GENERATOR",
+////        table = "MY_SEQUENCES",
+////        pkColumnValue = "MEMBER_SEQ", allocationSize = 1
+////)
+//public class Member {
+//
+//    @Id
+////    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+////    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
+//    private Long id;
+//
+//    @Column(name = "name", nullable = false) // DB 컬럼명은 name, 객체 이름은 username으로 사용
+//    private String username;
+//
+//    public Member() {
+//
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//}
