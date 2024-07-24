69f31ca6be5b46ce0fbc553daa3e470200a726a8
alpacaive <w6352800@naver.com>
2024-07-24 20:42:25 +0900

    양방향 연관관계와 연관관계의 주인1 - 기본

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,50 +1,51 @@
-//package hellojpa;
-//
-//import jakarta.persistence.*;
-//
-//import java.time.LocalDate;
-//import java.time.LocalDateTime;
-//import java.util.Date;
-//
-//@Entity
-//@SequenceGenerator(
-//        name = "MEMBER_SEQ_GENERATOR",
-//        sequenceName = "MEMBER_SEQ", // 매핑할 데이터베이스 시퀀스 이름
-//        initialValue = 1, allocationSize = 50 // DB에 미리 50개 올려놓고 그 개수만큼 미리 쓰는 방식, 여러 웹 서버가 있어도 동시성 이슈 없이 다양한 문제들이 해결됨
-//)
-////@TableGenerator(
-////        name = "MEMBER_SEQ_GENERATOR",
-////        table = "MY_SEQUENCES",
-////        pkColumnValue = "MEMBER_SEQ", allocationSize = 1
-////)
-//public class Member {
-//
-//    @Id
-////    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
-////    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
-//    private Long id;
-//
-//    @Column(name = "name", nullable = false) // DB 컬럼명은 name, 객체 이름은 username으로 사용
-//    private String username;
-//
-//    public Member() {
-//
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//}
+package hellojpa;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_gen")
+    @SequenceGenerator(name = "member_seq_gen", sequenceName = "MEMBER_SEQ", allocationSize = 1)
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,31 +1,31 @@
-package jpabook.jpashop;
-
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
-import jpabook.jpashop.domain.Order;
-
-public class JpaMain {
-
-    public static void main(String[] args) {
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 트랜잭션 안에서 되어야 한다
-
-        tx.begin();
-
-        try {
-
-
-            tx.commit();
-        } catch (Exception e) {
-            tx.rollback();
-        } finally {
-            em.close();
-        }
-
-        emf.close();
-    }
-}
+//package jpabook.jpashop;
+//
+//import jakarta.persistence.EntityManager;
+//import jakarta.persistence.EntityManagerFactory;
+//import jakarta.persistence.EntityTransaction;
+//import jakarta.persistence.Persistence;
+//import jpabook.jpashop.domain.Order;
+//
+//public class JpaMain {
+//
+//    public static void main(String[] args) {
+//
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+//        EntityManager em = emf.createEntityManager();
+//        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 트랜잭션 안에서 되어야 한다
+//
+//        tx.begin();
+//
+//        try {
+//
+//
+//            tx.commit();
+//        } catch (Exception e) {
+//            tx.rollback();
+//        } finally {
+//            em.close();
+//        }
+//
+//        emf.close();
+//    }
+//}
