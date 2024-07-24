67f24afe420a9c8895141799a67efd0aca019a8c
alpacaive <w6352800@naver.com>
2024-07-25 02:59:14 +0900

    일대다 [1 : N]

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,55 +1,39 @@
-//package hellojpa;
-//
-//import jakarta.persistence.*;
-//
-//import java.util.List;
-//
-//public class JpaMain {
-//
-//    public static void main(String[] args) {
-//
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-//        EntityManager em = emf.createEntityManager();
-//        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 트랜잭션 안에서 되어야 한다
-//
-//        tx.begin();
-//
-//        try {
-//
-//            // 저장
-//
-//            Team team = new Team();
-//            team.setName("TeamA");
-//            em.persist(team);
-//
-//            Member member = new Member();
-//            member.setUsername("member1");
-////            member.changeTeam(team);
-//            em.persist(member);
-//
-//            team.addMember(member);
-//
-////            team.getMembers().add(member); // Member에 setTeam에 추가
-//
-////            em.flush();
-////            em.clear();
-//
-//            Team findTeam = em.find(Team.class, team.getId());
-//            List<Member> members = findTeam.getMembers();
-//
-//            System.out.println("====================");
-//            for (Member m : members) {
-//                System.out.println("m = " + m.getUsername());
-//            }
-//            System.out.println("====================");
-//
-//            tx.commit();
-//        } catch (Exception e) {
-//            tx.rollback();
-//        } finally {
-//            em.close();
-//        }
-//
-//        emf.close();
-//    }
-//}
+package hellojpa;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 트랜잭션 안에서 되어야 한다
+
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+
+            em.persist(member);
+
+            Team team = new Team();
+            team.setName("teamA");
+            team.getMembers().add(member);
+
+            em.persist(team);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
