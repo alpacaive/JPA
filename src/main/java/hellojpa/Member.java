3a40838a9c2809dc8a51fa8694bd666442d244af
alpacaive <w6352800@naver.com>
2024-07-25 01:15:10 +0900

    실전 예제2 - 연관관계 매핑 시작 - 이건 예제이기 때문에 양방향으로 설계했지만 핵심은 단방향을 잘 설계하는 게 가장 중요하다

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,56 +1,56 @@
-package hellojpa;
-
-import jakarta.persistence.*;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-
-@Entity
-public class Member {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_gen")
-    @SequenceGenerator(name = "member_seq_gen", sequenceName = "MEMBER_SEQ", allocationSize = 1)
-    @Column(name = "MEMBER_ID")
-    private Long id;
-
-    @Column(name = "USERNAME")
-    private String username;
-
-//    @Column(name = "TEAM_ID")
-//    private Long teamId;
-
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-
-//    public void changeTeam(Team team) {
-//        this.team = team;
-//        team.getMembers().add(this); // 연관관게 편의 메서드
+//package hellojpa;
+//
+//import jakarta.persistence.*;
+//
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import java.util.Date;
+//
+//@Entity
+//public class Member {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_gen")
+//    @SequenceGenerator(name = "member_seq_gen", sequenceName = "MEMBER_SEQ", allocationSize = 1)
+//    @Column(name = "MEMBER_ID")
+//    private Long id;
+//
+//    @Column(name = "USERNAME")
+//    private String username;
+//
+////    @Column(name = "TEAM_ID")
+////    private Long teamId;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "TEAM_ID")
+//    private Team team;
+//
+//    public Long getId() {
+//        return id;
 //    }
-}
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public Team getTeam() {
+//        return team;
+//    }
+//
+//    public void setTeam(Team team) {
+//        this.team = team;
+//    }
+//
+////    public void changeTeam(Team team) {
+////        this.team = team;
+////        team.getMembers().add(this); // 연관관게 편의 메서드
+////    }
+//}
--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -1,51 +1,51 @@
-package hellojpa;
-
-import jakarta.persistence.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Entity
-public class Team {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "team_seq_gen")
-    @SequenceGenerator(name = "team_seq_gen", sequenceName = "TEAM_SEQ", allocationSize = 1)
-    @Column(name = "TEAM_ID")
-    private Long id;
-
-    private String name;
-
-    @OneToMany(mappedBy = "team") // member의 team에 매핑 되어있다는 뜻, 조회만 가능, 연관관계 주인은 Member의 team
-    private List<Member> members = new ArrayList<>();
-
-    public void addMember(Member member) {
-        member.setTeam(this);
-        members.add(member);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Member> getMembers() {
-        return members;
-    }
-
-    public void setMembers(List<Member> members) {
-        this.members = members;
-    }
-
-}
+//package hellojpa;
+//
+//import jakarta.persistence.*;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Entity
+//public class Team {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "team_seq_gen")
+//    @SequenceGenerator(name = "team_seq_gen", sequenceName = "TEAM_SEQ", allocationSize = 1)
+//    @Column(name = "TEAM_ID")
+//    private Long id;
+//
+//    private String name;
+//
+//    @OneToMany(mappedBy = "team") // member의 team에 매핑 되어있다는 뜻, 조회만 가능, 연관관계 주인은 Member의 team
+//    private List<Member> members = new ArrayList<>();
+//
+//    public void addMember(Member member) {
+//        member.setTeam(this);
+//        members.add(member);
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public List<Member> getMembers() {
+//        return members;
+//    }
+//
+//    public void setMembers(List<Member> members) {
+//        this.members = members;
+//    }
+//
+//}
