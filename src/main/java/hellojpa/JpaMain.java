3e48a656cf0688df2a7425a791c4fc70f15f41a7
alpacaive <w6352800@naver.com>
2024-07-25 17:21:45 +0900

    실전 예제3 - 다양한 연관관계 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,39 +1,39 @@
-package hellojpa;
-
-import jakarta.persistence.*;
-
-import java.util.List;
-
-public class JpaMain {
-
-    public static void main(String[] args) {
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 트랜잭션 안에서 되어야 한다
-
-        tx.begin();
-
-        try {
-
-            Member member = new Member();
-            member.setUsername("member1");
-
-            em.persist(member);
-
-            Team team = new Team();
-            team.setName("teamA");
-            team.getMembers().add(member);
-
-            em.persist(team);
-
-            tx.commit();
-        } catch (Exception e) {
-            tx.rollback();
-        } finally {
-            em.close();
-        }
-
-        emf.close();
-    }
-}
+//package hellojpa;
+//
+//import jakarta.persistence.*;
+//
+//import java.util.List;
+//
+//public class JpaMain {
+//
+//    public static void main(String[] args) {
+//
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+//        EntityManager em = emf.createEntityManager();
+//        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 트랜잭션 안에서 되어야 한다
+//
+//        tx.begin();
+//
+//        try {
+//
+//            Member member = new Member();
+//            member.setUsername("member1");
+//
+//            em.persist(member);
+//
+//            Team team = new Team();
+//            team.setName("teamA");
+//            team.getMembers().add(member);
+//
+//            em.persist(team);
+//
+//            tx.commit();
+//        } catch (Exception e) {
+//            tx.rollback();
+//        } finally {
+//            em.close();
+//        }
+//
+//        emf.close();
+//    }
+//}
--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,39 +1,39 @@
-//package jpabook.jpashop;
-//
-//import jakarta.persistence.EntityManager;
-//import jakarta.persistence.EntityManagerFactory;
-//import jakarta.persistence.EntityTransaction;
-//import jakarta.persistence.Persistence;
-//import jpabook.jpashop.domain.Order;
-//import jpabook.jpashop.domain.OrderItem;
-//
-//public class JpaMain {
-//
-//    public static void main(String[] args) {
-//
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-//        EntityManager em = emf.createEntityManager();
-//        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 트랜잭션 안에서 되어야 한다
-//
-//        tx.begin();
-//
-//        try {
-//            Order order = new Order();
-////            order.addOrderItem(new OrderItem());
-//            em.persist(order);
-//
-//            OrderItem orderItem = new OrderItem();
-//            orderItem.setOrder(order);
-//
-//            em.persist(orderItem);
-//
-//            tx.commit();
-//        } catch (Exception e) {
-//            tx.rollback();
-//        } finally {
-//            em.close();
-//        }
-//
-//        emf.close();
-//    }
-//}
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 트랜잭션 안에서 되어야 한다
+
+        tx.begin();
+
+        try {
+            Order order = new Order();
+//            order.addOrderItem(new OrderItem());
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
