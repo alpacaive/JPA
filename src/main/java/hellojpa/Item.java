2e94066b3c1bef0279b7c7a1c44531aa642634fe
alpacaive <w6352800@naver.com>
2024-07-26 16:58:46 +0900

    프록시

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -1,41 +1,41 @@
-//package hellojpa;
-//
-//import jakarta.persistence.*;
-//
-//@Entity
-//@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-//@DiscriminatorColumn // DTYPE 컬럼 생김, 디폴트값은 엔티티명 Movie, Album, Book
-//public abstract class Item { // 모든 전략에서 추상 클래스로 만드는게 맞았는데 까먹고 이제 만든거임
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "item_seq_gen")
-//    @SequenceGenerator(name = "item_seq_gen", sequenceName = "ITEM_SEQ", allocationSize = 1)
-//    private Long id;
-//
-//    private String name;
-//    private int price;
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public int getPrice() {
-//        return price;
-//    }
-//
-//    public void setPrice(int price) {
-//        this.price = price;
-//    }
-//}
+package hellojpa;
+
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@DiscriminatorColumn // DTYPE 컬럼 생김, 디폴트값은 엔티티명 Movie, Album, Book
+public abstract class Item { // 모든 전략에서 추상 클래스로 만드는게 맞았는데 까먹고 이제 만든거임
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "item_seq_gen")
+    @SequenceGenerator(name = "item_seq_gen", sequenceName = "ITEM_SEQ", allocationSize = 1)
+    private Long id;
+
+    private String name;
+    private int price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,39 +1,51 @@
-//package hellojpa;
-//
-//import jakarta.persistence.*;
-//
-//import java.time.LocalDateTime;
-//import java.util.List;
-//
-//public class JpaMain {
-//
-//    public static void main(String[] args) {
-//
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-//        EntityManager em = emf.createEntityManager();
-//        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 트랜잭션 안에서 되어야 한다
-//
-//        tx.begin();
-//
-//        try {
-//
-//            Member member = new Member();
-//            member.setUsername("user1");
-//            member.setCreatedBy("Kim");
-//            member.setCreatedDate(LocalDateTime.now());
-//
-//            em.persist(member);
-//
-//            em.flush();
-//            em.clear();
-//
-//            tx.commit();
-//        } catch (Exception e) {
-//            tx.rollback();
-//        } finally {
-//            em.close();
-//        }
-//
-//        emf.close();
-//    }
-//}
+package hellojpa;
+
+import jakarta.persistence.*;
+import org.hibernate.Hibernate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction(); // JPA의 모든 데이터 변경은 트랜잭션 안에서 되어야 한다
+
+        tx.begin();
+
+        try {
+
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            
+            em.persist(member1);
+
+            em.flush();
+            em.clear();
+            
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass()); // Proxy
+
+//            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
+            Hibernate.initialize(refMember); // 강제 초기화
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    private static void logic(Member m1, Member m2) {
+        System.out.println("m1 == m2 : " + (m1 instanceof Member));
+        System.out.println("m1 == m2 : " + (m2 instanceof Member));
+    }
+
+}
