9a0f3ea36212d6fe0c5e22df538db42095a642fb
alpacaive <w6352800@naver.com>
2024-07-29 20:21:31 +0900

    영속성 전이 - cascade

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,38 +18,16 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team team2 = new Team();
-            team2.setName("teamB ");
-            em.persist(team2);
+            Parent parent = new Parent();
+//            parent.addChild(child1);
+//            parent.addChild(child2);
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
-            em.persist(member1);
-
-            Member member2 =  new Member();
-            member2.setUsername("member2");
-            member2.setTeam(team2);
-            em.persist(member2);
-
-            em.flush();
-            em.clear();
-            
-//            Member m = em.find(Member.class, member1.getId());
-//
-//            System.out.println("m = " + m.getTeam().getClass());
-//            System.out.println("====================");
-//            System.out.println("teamName : " + m.getTeam().getName());
-//            System.out.println("====================");
-
-            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class) // 패치조인, 뒤에서 자세히 배움
-                    .getResultList();
-            // SQL : select * from member
-            // SQL : select * from team where TEAM_ID = xxx
+            em.persist(parent);
+            em.persist(child1);
+            em.persist(child2);
 
             tx.commit();
         } catch (Exception e) {
---
+++ src/main/java/hellojpa/Parent.java
@@ -0,0 +1,41 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Parent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "parent_seq_gen")
+    @SequenceGenerator(name = "parent_seq_gen", sequenceName = "PARENT_SEQ", allocationSize = 1)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<Child> childList = new ArrayList<>();
+
+    public void addChild(Child child) {
+        childList.add(child);
+        child.setParent(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
