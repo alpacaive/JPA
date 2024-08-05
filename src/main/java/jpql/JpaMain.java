1a46d7d752a1d7cf3d94b7137fc1e782d0a206e0
alpacaive <w6352800@naver.com>
2024-08-06 00:00:52 +0900

    페치 조인1 - 기본

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -18,43 +18,70 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-//            // 상태 필드 : 경로 탐색의 끝. 탐색 더이상 불가
-//            String query = "select m.username From Member m";
+//            // 엔티티 페치조인
+//            String query = "select m From Member m join fetch m.team";
 //
-//            // 단일 값 연관 경로 : 묵시적 내부 조인(inner join) 발생, 탐색 더 가능 -> 실무에서는 묵시적 내부 조인이 일어나게 짜면 안된다. 운영 어려워진다
-//            String query = "select m.team From Member m";
-//
-//            List<String> result = em.createQuery(query, String.class)
+//            List<Member> result = em.createQuery(query, Member.class)
 //                    .getResultList();
 //
-//            for (String s : result) {
-//                System.out.println("s = " + s);
+//            for (Member member : result) {
+//                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
 //            }
 
-            // 컬렉션 값 연관 경로 : 묵시적 내부 조인 발생, 탐색 더이상 불가능
-            String query = "select t.members From Team t";
+//            // 컬렉션 페치 조인 → 하이버네이트6 부터는 DISTINCT 명령어를 사용하지 않아도 애플리케이션에서 중복 제거가 자동으로 적용된다
+//            String query = "select distinct t From Team t join fetch t.members";
+//
+//            List<Team> result = em.createQuery(query, Team.class)
+//                    .getResultList();
+//
+//            System.out.println("result = " + result.size());
+//
+//            for (Team team : result) {
+//                System.out.println("team = " + team.getName() + " | members = " + team.getMembers().size());
+//                for (Member member : team.getMembers()) {
+//                    System.out.println("-> member = " + member);
+//                }
+//            }
 
-            List<Collection> result = em.createQuery(query, Collection.class)
+            // 일반 조인 -> 팀 데이터만 가져옴 페치 조인이 아니기 때문 (페치 조인이랑 차이 볼려고 작성해봄)
+            String query = "select t From Team t join t.members m";
+
+            List<Team> result = em.createQuery(query, Team.class)
                     .getResultList();
 
-            for (Object o : result) {
-                System.out.println("o = " + o);
+            System.out.println("result = " + result.size());
+
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + " | members = " + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
             }
 
             tx.commit();
